52e639671f0245bb3a907499f3b233e9b4162f6a
Max Berger <maxamillianberger@gmail.com>
2017-03-13 19:34:35 -0700

    Add Files for a Home Page
    Also trying to fix deployment side bugs

--- Client/app.js
+++ Client/app.js
@@ -1,11 +1,14 @@
-// angular.module('artGallery', [])
-// .config(function ($routeProvider, $httpProvider) {
-//   $routeProvider
-//     .when('/home', {
-//       templateUrl: 'client/index.html',
-//       // controller: ''
-//     })
-//     .otherwise({
-//       redirectTo: '/home'
-//     });
-// });
+angular.module('gallery', [
+  'gallery.home',
+  'ngRoute'
+])
+.config(function ($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'client/home/home.html',
+      controller: 'HomeController'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
+});
